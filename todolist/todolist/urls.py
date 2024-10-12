82094a88bab816a64e19ede72675fb14006479b9
nicky-nacky <n.abarinov@yandex.ru>
2024-10-12 22:44:09 +0300

    add task_list html

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todolist.todolist import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.task_list, name='task_list'),
 ]
